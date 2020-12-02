629969fdca92c694648c1615436b6bbe79fecfac
sheknowshowtocode <74694750+sheknowshowtocode@users.noreply.github.com>
2020-12-01 20:32:04 -0500

    Update 1-1.js

--- 1-1.js
+++ 1-1.js
@@ -1,11 +1,11 @@
 function getHour(name) {
    var d = new Date();
    var n = d.getHours();
-  if (n < 4 ) {
+  if (n <= 4 ) {
        console.log('доброй ночи ' + name, n)
-    } else if (n < 9) {
+    } else if (n <= 9) {
        console.log('доброе утро ' + name)
-    } else if (n < 16) {
+    } else if (n <= 16) {
         console.log('добрый день, ' + name)
     } else {
       console.log('добрый вечер, ' + name)
